638811da1e536885fbb1880c026511c1848804f2
MuchinaWamburia <lucymuchina20@gmail.com>
2023-01-15 11:19:30 +0300

    done

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,30 +1,41 @@
 import logo from "../assets/logo.svg";
 
 const blogData = {
-  name: "Underreacted",
+  name: "Overreacted",
   image: logo,
-  about: "A blog about learning React",
+  about: "Personal blog by Dan Abramov .I explain with words and code.",
   posts: [
     {
       id: 1,
-      title: "Components 101",
-      date: "December 15, 2020",
-      preview: "Setting up the building blocks of your site",
-      minutes: 5,
+      title: "npm audit: Broken by Design",
+      date:" July 7, 2021 • ☕️☕️☕️ 14 min read",
+      preview: "Found 99 vulnerabilities (84 moderately irrelevant, 15 highly irrelevant)",
+      minutes: 14,
     },
     {
       id: 2,
-      title: "React Data Flow",
-      date: "December 11, 2020",
-      preview: "Passing props is never passé",
-      minutes: 15,
+      title: "Before You memo()",
+      date: "February 23, 2021 ",
+      preview: "Rendering optimizations that come naturally.",
+      minutes: 5,
     },
     {
       id: 3,
-      title: "Function vs Class Components",
-      preview: "React, meet OOJS.",
-      minutes: 47,
+      title: "The WET Codebase",
+      preview: "Come waste your time with me.",
+      minutes: 1,
     },
+    {id: 4,
+    title: "Goodbye,Clean Code",
+    date:"January 11,2020",
+    preview: "Let clean code guide you. Then let it go.",
+    minutes: 5,
+    },
+    {id:5,
+    title:"My Decade in Review",
+    date:"January1,2020",
+  preview:"A personal reflection",
+minutes:26}
   ],
 };
 
